4e74d71fe10064e7d8c8924350bda0ac31d1204f
Andy <andy@upthere.com>
2014-03-02 15:46:40 -0800

    Initial Commit

---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by Andy Wang on 3/2/14.
+//  Copyright (c) 2014 Andy Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ JenkinsTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JenkinsTest
+//
+//  Created by Andy Wang on 3/2/14.
+//  Copyright (c) 2014 Andy Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
